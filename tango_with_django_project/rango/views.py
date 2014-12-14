e315f694fa8505b8de8afa8c7cb5f2ba71fbca16
Rohit Vernekar <rohitvernekar007@gmail.com>
2014-12-14 21:27:53 +0530

    Update index & about views

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango sas hello world")
+    return HttpResponse("Rango says: hello world  <a href='/rango/about'>About</a>")
+
+def about(request):
+    return HttpResponse("Rango says: Here is the about page, to get back to Home click <a href='/rango'>Home</a>")
