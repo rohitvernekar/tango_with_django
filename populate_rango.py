41e1ae560c1bfdd6a790c6b0903580971c195036
Rohit Vernekar <rohitvernekar007@gmail.com>
2014-12-20 23:08:50 +0530

    Populating views & likes

--- populate_rango.py
+++ populate_rango.py
@@ -1,7 +1,7 @@
 import os
 
 def populate():
-    python_cat = add_cat('Python')
+    python_cat = add_cat('Python',128,64)
     
     add_page(cat=python_cat,
         title="Official Python tutorial",
@@ -9,13 +9,15 @@ def populate():
 
     add_page(cat=python_cat,
         title="How to think like a computer Scientist",
-        url='http://www.greenteapress.com/thinkpython/*')
+        url='http://www.greenteapress.com/thinkpython/')
 
     add_page(cat=python_cat,
         title="Learn Python in 10 Minutes",
-        url='http://www.korokithakis.net/tutorials/python/*')
+        url='http://www.korokithakis.net/tutorials/python/')
     
-    django_cat = add_cat("Django")
+    django_cat = add_cat("Django",64,32)
+
+    
 
     add_page(cat=django_cat,
         title="Official Django Tutorial",
@@ -25,8 +27,7 @@ def populate():
         title="How to Tango with Django",
         url="http://www.tangowithdjango.com/")
 
-    frame_cat = add_cat("Other Frameworks")
-
+    frame_cat = add_cat("Other Frameworks",32,16)
     add_page(cat=frame_cat,
         title="Bottle",
         url="http://bottlepy.org/docs/dev/")
@@ -40,14 +41,13 @@ def populate():
         for p in Page.objects.filter(category=c):
             print "- {0} - {1}".format(str(c), str(p))
 
-def add_page(cat, title, url, views=0):
-    p = Page.objects.get_or_create(category=cat, title=title, url=url, views=views)[0]
+def add_page(cat, title, url):
+    p = Page.objects.get_or_create(category=cat, title=title, url=url)[0]
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name,views,likes,type):
+    c = Category.objects.get_or_create(name=name,views=views,likes=likes)[0]
     return c
-
 
 # Start execution here!
 if __name__=='__main__':
