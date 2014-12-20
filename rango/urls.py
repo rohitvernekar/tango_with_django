d7425cadd0da905a57a7090b646272494124b894
Rohit Vernekar <rohitvernekar007@gmail.com>
2014-12-20 14:05:42 +0530

    Urls for index & about page

--- rango/urls.py
+++ rango/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 from rango import views
 
 urlpatterns = patterns('',
-        #url(r'^index/$',views.index),
-        #url(r'^about/$',views.about),
+        url(r'^index/$',views.index),
+        url(r'^about/$',views.about),
     )
