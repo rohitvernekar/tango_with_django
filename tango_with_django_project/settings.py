261f11841c13b03e8065d0bf78252f9dc91265c5
Rohit Vernekar <rohitvernekar007@gmail.com>
2014-12-20 11:43:48 +0530

    Sqlite3 Database configuration

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,10 +14,12 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+DATABASE_PATH=os.path.join(PROJECT_PATH,'rango.db')
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'DATABASE_PATH',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
