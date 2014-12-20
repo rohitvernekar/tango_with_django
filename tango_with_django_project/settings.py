8aa391a72bde0781c638932271376bb4df9ae867
Rohit Vernekar <rohitvernekar007@gmail.com>
2014-12-20 13:11:29 +0530

    Database name error

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -19,7 +19,7 @@ DATABASE_PATH=os.path.join(PROJECT_PATH,'rango.db')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'DATABASE_PATH',                      # Or path to database file if using sqlite3.
+        'NAME': DATABASE_PATH,                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
