ab5288fe89d38399f31f272cec70cbcb88ea83a2
Rohit Vernekar <rohitvernekar007@gmail.com>
2014-12-20 14:06:56 +0530

    Configuring admin interface & updating rango db

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 from rango import views
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -15,9 +15,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    #url(r'^rango/', include('rango.urls')),
-    url(r'^index/$',views.index),
-    url(r'^about/$',views.about),
+    url(r'^rango/', include('rango.urls')),
+    url(r'^admin/',include(admin.site.urls)),
 )
 
 if settings.DEBUG:
