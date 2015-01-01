50977346795d64742f457b1125bbef15ec5b5d76
Rohit Vernekar <rohitvernekar007@gmail.com>
2015-01-01 21:22:57 +0530

    Added Registration functionality

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm
+from rango.forms import UserForm, UserProfileForm
 
 def encode_url(str):
     return str.replace(' ', '_')
@@ -82,3 +83,38 @@ def add_page(request, category_name_url):
              {'category_name_url': category_name_url,
              'category_name': category_name, 'form':form},
              context)
+
+def register(request):
+    context = RequestContext(request)
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+        
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+             
+            profile=profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+            registered = True
+
+
+        else:
+            print user_form.errors, profile_form.errors
+
+    else:
+        user_form=UserForm()
+        profile_form=UserProfileForm()
+
+
+    return render_to_response(
+            'rango/register.html',
+            {'user_form': user_form, 'profile_form':profile_form,'registered':registered},
+            context)
